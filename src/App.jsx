c48b00422b3000643ada50ec6dfa4482845fb4c8
pabasara-1120 <jayasekeramp@gmail.com>
2024-08-17 12:58:14 +0530

    add chat component

--- src/App.jsx
+++ src/App.jsx
@@ -50,20 +50,60 @@ const MatchesList = () => (
   </div>
 );
 
+const ChatScreen = () => {
+  const [input,setInput] = useState('');
+
+  const handleSend = ()=>{
+    if(input.trim()){
+      console.log(input)
+      setInput('');
+    }
+    
+
+  }
+
+  return (
+    <div className="rounded-lg overflow-hidden bg-white shadow-lg p-4">
+      <h2 className="text-2xl font-bold mb-4">Chat with Foo</h2>
+      <div className="h-[50vh] border rounded overflow-y-auto mb-4 p-2">
+        {["Hi", "How are you"].map((message, index) => (
+          <div key={index} className="text-left">
+            <div className="mb-4 p-2 rounded bg-gray-100">{message}</div>
+          </div>
+        ))}
+      </div>
+      <div className='flex'>
+        <input 
+        type='text' 
+        value={input}
+        onChange={(e)=> setInput(e.target.value)}
+        className='border flex-1 rounded p-2 mr-2'
+        placeholder='type a message'></input>
+        <button 
+        className='bg-blue-500 text-white rounded p-2'
+        onClick={handleSend}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+
+
 
 
 
 function App() {
 
-  const [currentScreen, setCurrentScreen] = useState("matches");
+  const [currentScreen, setCurrentScreen] = useState("chat");
 
   const renderScreen =() =>{
     switch (currentScreen) {
       case "profile":
         return <ProfileSelector/>;
       case "matches":
-        return <MatchesList/>
-        
+        return <MatchesList/>;
+      case "chat":
+        return <ChatScreen/>;
         
     
       
